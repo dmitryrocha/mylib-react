6095d1c772e3040cf3cb47ebe80e504d0f78d412
dmitryrocha <dmitryrocha@gmail.com>
2022-11-28 20:24:36 -0300

    sinalização de livro alugado adicionado

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,10 +1,17 @@
-import react from 'react';
+
 
 const Modal = ({show, item, onClose}) => {
     if(!show) {
         return null;
     }
     let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+    const avaliable = Boolean(Math.round(Math.random() < 0.5))
+    let isAvaliable = "";
+    if(avaliable) {
+        isAvaliable = "Disponível";
+    } else {
+        isAvaliable = "Alugado"
+    }
     return(
         <>
             <div className="overlay">
@@ -16,6 +23,7 @@ const Modal = ({show, item, onClose}) => {
                             <h1>{item.volumeInfo.title}</h1>
                             <h3>{item.volumeInfo.authors}</h3><br/>
                             <h4>{item.volumeInfo.publisher} <span>{item.volumeInfo.publishedDate}</span></h4><br/>
+                            <h4>{isAvaliable}</h4><br/>
                             <a href={item.volumeInfo.previewLink}><button>Mais</button></a>
                         </div>
                     </div>
